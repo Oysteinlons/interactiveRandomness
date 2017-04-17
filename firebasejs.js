5d0a556b11bd2a19812a99e17cc4415866dbfa5d
Oysteinlons <oysteinlons@gmail.com>
2017-04-17 22:10:14 +0200

    awdawd
    tawd

--- firebasejs.js
+++ firebasejs.js
@@ -1,5 +1,8 @@
 var firebaseRef = firebase.database().ref();
 
+var purchaseCounter = 0;
+var timeCounter = 0;
+var price = 1;
 var testTall = 0;
 var number = 0;
 var currentLink = 0;
@@ -13,27 +16,45 @@ firebaseRef.child("linkCounter").once('value').then(function(snapshot){
 	});
 
 setInterval(checkForUpdate, 10);
+setInterval(updatePrice, 1);
 
 function checkForUpdate(){
 
 		currentLink = document.getElementById("pic1").src;
-		console.log("The link showed in <img> tags is: " +currentLink)
+		//console.log("The link showed in <img> tags is: " +currentLink)
 
 		if(!(currentLink == firebaseLink)){
-			console.log(currentLink+ " and " +firebaseLink+ " is the same");
+		//	console.log(currentLink+ " and " +firebaseLink+ " is the same");
 			getPicFromFirebase();
 		}
 
 		firebaseRef.child("imgSRC").once('value').then(function(snapshot){
 
 		firebaseLink = snapshot.val();
-		console.log("The link on firebase is: " +firebaseLink);
+		//console.log("The link on firebase is: " +firebaseLink);
 
 	});
 }
 
+function updatePrice(){
+
+timeCounter += 1;
+if(timeCounter == 10){
+	price += purchaseCounter*purchaseCounter;
+	timeCounter = 0;
+	console.log(purchaseCounter)
+	purchaseCounter = 0;
+}
+
+price -= 0.005;
+document.getElementById("price").innerHTML = price.toFixed(3);
+}
+
 function updatePicOnFirebase(){
+
+	purchaseCounter += 1;
 	i += 1;
+	price += 1;
 	
 	firebaseRef.child("linksSubmitted").child(i).once('value').then(function(snapshot){
 		lastLinkSubmitted = snapshot.val();
@@ -44,7 +65,7 @@ function updatePicOnFirebase(){
 	firebaseRef.child("imgSRC").set(newInfo1);
 	firebaseRef.child("linkCounter").set(i);
 	//lag en if setning her og sjekk om newInfo1 er lik den siste linken
-	// som ble pasta i firebase
+	// som ble pasta i firebase 
 	
 	firebaseRef.child("linksSubmitted").child(i).set(newInfo1);
 	
